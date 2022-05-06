ce01e7dc405a9052a57500656431fbb3bf22557d
arkiver <arkiver@protonmail.com>
2022-05-06 18:37:11 +0200

    Version 20220605.01. Support GNU Wget 1.21.3-at.20220503.02. Fix killing crawl when items cannot be queued.

--- grab.lua
+++ grab.lua
@@ -10,6 +10,7 @@ local warc_file_base = os.getenv("warc_file_base")
 local url_count = 0
 local downloaded = {}
 local abortgrab = false
+local killgrab = false
 local exit_url = false
 
 if urlparse == nil or http == nil then
@@ -58,6 +59,11 @@ for pattern in patterns_file:lines() do
   end
 end
 patterns_file:close()
+
+kill_grab = function(item)
+  io.stdout:write("Aborting crawling.\n")
+  killgrab = true
+end
 
 read_file = function(file, bytes)
   if not bytes then
@@ -142,7 +148,7 @@ queue_url = function(urls_queue, url)
   url = string.match(url, "^([^<]+)")
   url = string.match(url, "^([^\\]+)")
   if not queued_urls[url] and not urls_queue[url] then
-    if find_path_loop(url, 3) then
+    if find_path_loop(url, 2) then
       return false
     end
     urls_queue[url] = true
@@ -204,7 +210,7 @@ wget.callbacks.download_child_p = function(urlpos, parent, depth, start_url_pars
     return true
   end
 
-  if find_path_loop(url, 3) then
+  if find_path_loop(url, 2) then
     return false
   end
 
@@ -441,6 +447,10 @@ wget.callbacks.httploop_result = function(url, err, http_stat)
   io.stdout:write(url_count .. "=" .. status_code .. " " .. url["url"] .. "  \n")
   io.stdout:flush()
 
+  if killgrab then
+    return wget.actions.ABORT
+  end
+
   if redirect_domains["done"] then
     redirect_domains = {}
     redirect_urls = {}
@@ -502,6 +512,9 @@ wget.callbacks.finish = function(start_time, end_time, wall_time, numurls, total
     local tries = 0
     local maxtries = 4
     while tries < maxtries do
+      if killgrab then
+        return false
+      end
       local body, code, headers, status = http.request(
         "https://legacy-api.arpa.li/backfeed/legacy/" .. key,
         newurls .. "\0"
@@ -518,7 +531,7 @@ wget.callbacks.finish = function(start_time, end_time, wall_time, numurls, total
       tries = tries + 1
     end
     if tries == maxtries then
-      abortgrab = true
+      kill_grab()
     end
   end
 
@@ -558,6 +571,9 @@ wget.callbacks.finish = function(start_time, end_time, wall_time, numurls, total
 end
 
 wget.callbacks.before_exit = function(exit_status, exit_status_string)
+  if killgrab then
+    return wget.exits.IO_FAIL
+  end
   if abortgrab then
     return wget.exits.IO_FAIL
   end
